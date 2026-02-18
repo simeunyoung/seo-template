a09779460e9c1e25fc074fe4ab2e2ab8b52a70f7
은영 <duddl4766@gmail.com>
2026-02-18 21:40:48 +0900

    update url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://YOURPROJECT.vercel.app",
+  site: "https://seo-template-eta.vercel.app",
   integrations: [sitemap()],
 });
